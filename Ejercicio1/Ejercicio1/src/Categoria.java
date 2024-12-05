fe71d679b317ba92d9d4a539e343d5582f9d01ea
BrayanRodriguez <brayan.rodriguez06@usc.edu.co>
2024-12-05 01:07:23 -0500

    Delete Ejercicio1/Ejercicio1 directory

--- Ejercicio1/Ejercicio1/src/Categoria.java
+++
@@ -1,4 +0,0 @@
-//Brayan Rodriguez Fernandez,Angel Gabriel Villamizar Caicedo,Juan Sebastian Mosquera Murillo
-public enum Categoria {
-    ALIMENTOS, ELECTRONICA, ROPA, JUGUETES;
-}
--- Ejercicio1/Ejercicio1/src/OperacionesTienda.java
+++
@@ -1,6 +0,0 @@
-//Brayan Rodriguez Fernandez,Angel Gabriel Villamizar Caicedo,Juan Sebastian Mosquera Murillo
-public interface OperacionesTienda {
-    void agregarProducto(String nombre, double precio, int cantidad);
-    void venderProducto(String nombre, int cantidad);
-    void mostrarInventario();
-}
